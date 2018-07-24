151e944454fee2fff9229bb0bd5e32bad7dcf946
qqchao <qc9201@sina.com>
2018-07-24 23:53:40 +0800

    2018年7月24日

---
+++ leetCode/src/main/java/explore/level1/string/LongestCommonPrefix.java
@@ -0,0 +1,42 @@
+package explore.level1.string;
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * 示例 1:
+ *
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ *
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 说明:
+ *
+ * 所有输入只包含小写字母 a-z 。
+ */
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) return "";
+        if(strs.length == 1) return strs[0];
+        String result = "";
+
+        for(int i = 0; i < strs[0].length(); i++){
+            char c = strs[0].charAt(i);
+            for(int j = 1; j < strs.length; j++){
+                if(strs[j].length() < (i+1) || strs[j].charAt(i) != c) return result;
+            }
+            result = result + c;
+        }
+        return result;
+    }
+}
---
+++ leetCode/src/main/java/problems/P326IsPowerOfThree.java
@@ -0,0 +1,55 @@
+package problems;
+
+/**
+ * 给定一个整数，写一个函数来判断它是否是 3 的幂次方。
+ *
+ * 示例 1:
+ *
+ * 输入: 27
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: 0
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入: 9
+ * 输出: true
+ * 示例 4:
+ *
+ * 输入: 45
+ * 输出: false
+ * 进阶：
+ * 你能不使用循环或者递归来完成本题吗？
+ */
+public class P326IsPowerOfThree {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree2(45));
+        System.out.println(isPowerOfThree2(81));
+    }
+
+    public boolean isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n>1){
+            if(n%3!=0){
+                return false;
+            }
+            n/=3;
+        }
+        return true;
+    }
+
+    /**
+     * 对数换底  妙极。。。
+     */
+    public static boolean isPowerOfThree2(int n) {
+        if(n<=0){
+            return false;
+        }
+
+        //  Math.log10(n)/Math.log10(3) 换底公式 结果就是log3(n)
+        return Math.log10(n)/Math.log10(3)%1==0;
+    }
+}
