a8bfd8e7fc9cc5088530f3b249bdca9018b03c19
qqchao <qinchao2@gome.com.cn>
2020-09-04 16:54:11 +0800

    full tree node

---
+++ src/main/java/cn/qqchao/other/treenode/TreeNode.java
@@ -0,0 +1,34 @@
+package cn.qqchao.other.treenode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+    TreeNode(int x, TreeNode p, TreeNode q) {
+        val = x;
+        left = p;
+        right = q;
+    }
+
+    public TreeNode(int[] array){
+        List<TreeNode> nodeList=new LinkedList<TreeNode>();
+
+        this.val = array[0];
+        nodeList.add(this);
+        for(int nodeIndex=1;nodeIndex<array.length;nodeIndex++){
+            nodeList.add(new TreeNode(array[nodeIndex]));
+        }
+        for(int parentIndex=0;parentIndex<=array.length/2-1;parentIndex++){
+            nodeList.get(parentIndex).left =nodeList.get(parentIndex*2+1);
+            //防止是非完全二叉树
+            if((parentIndex*2+2)<array.length) {
+                nodeList.get(parentIndex).right = nodeList.get(parentIndex * 2 + 2);
+            }
+        }
+    }
+}
