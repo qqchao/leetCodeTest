d490c696dc5f53adc6173784a894a2ec5dcd6950
qqchao <994340381@qq.com>
2022-07-08 21:28:41 +0800

    add psvm

--- src/main/java/cn/qqchao/leetcode/P9isPalindrome.java
+++ src/main/java/cn/qqchao/leetcode/P9isPalindrome.java
@@ -25,12 +25,16 @@ package cn.qqchao.leetcode;
  * 你能不将整数转为字符串来解决这个问题吗？
  *
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/palindrome-number
+ * 链接：<a href="https://leetcode-cn.com/problems/palindrome-number">...</a>
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class P9isPalindrome {
 
-    public boolean isPalindrome(int x) {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1234));
+        System.out.println(isPalindrome(1234321));
+    }
+    public static boolean isPalindrome(int x) {
         String s = String.valueOf(x);
         char[] chars = s.toCharArray();
         int n = chars.length-1;
